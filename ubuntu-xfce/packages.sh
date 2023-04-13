148d8cba76eaa3f0accb3859c5dd5240dc5516ce
kumalabsconsulting <kumalabsconsulting@gmail.com>
2023-04-13 09:22:24 +0200

    Fix

--- ubuntu-xfce/packages.sh
+++ ubuntu-xfce/packages.sh
@@ -7,6 +7,7 @@ echo "**** add docker gpg keystring ****"
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
+rm -rf /etc/apt/sources.list.d/docker.list
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 echo ""
 echo "**** add repo brave browser ****"
